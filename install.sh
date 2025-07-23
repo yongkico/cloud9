e8fcfe77e823a8b9179664baff4c72b40de17594
Yongki Hutagalung <58337410+yongkico@users.noreply.github.com>
2025-07-23 21:33:10 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -91,7 +91,7 @@ sudo docker exec Priv8-Tools /bin/bash -c "
   apt install php-curl -y && \
   cd /c9bins/.c9/ && \
   rm -rf user.settings && \
-  wget https://raw.githubusercontent.com/priv8-app/cloud9/refs/heads/main/user.settings
+  wget https://raw.githubusercontent.com/yongkico/cloud9/refs/heads/main/user.settings
 "
 if [ $? -eq 0 ]; then
   print_message "$GREEN" "✅ Cloud9 container configured successfully."
